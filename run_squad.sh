454131b5412f07a7437841515e243634f4519657
a414351664 <201424020211@chd.edu.cn>
2019-08-01 15:23:45 +0800

    config

--- run_squad.sh
+++ run_squad.sh
@@ -1,6 +1,6 @@
 export SQUAD_DIR=squad_data
 
-CUDA_VISIBLE_DEVICES=1,2 python run_squad.py \
+CUDA_VISIBLE_DEVICES=3,4 python run_squad.py \
   --bert_model 'bert-base-uncased' \
   --do_train \
   --do_predict \
@@ -9,7 +9,7 @@ CUDA_VISIBLE_DEVICES=1,2 python run_squad.py \
   --predict_file $SQUAD_DIR/dev-v1.1.json \
   --train_batch_size 12 \
   --learning_rate 6e-5 \
-  --num_train_epochs 10.0 \
+  --num_train_epochs 2.0 \
   --max_seq_length 384 \
   --doc_stride 128 \
   --overwrite_output_dir \
